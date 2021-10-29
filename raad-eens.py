c8c50696c64036da21ed3529df0d07b5572907ea
unknown <gorinchem1995@hotmail.com>
2021-10-29 13:28:21 +0200

    Aangepast

--- raad-eens.py
+++ raad-eens.py
@@ -1,5 +1,8 @@
-game = True
+from random import randint, randrange
 import time, os, sys
+game = True
+x = 0
+
 
 def clearScreen(sleepTime):
     time.sleep(sleepTime)
@@ -11,28 +14,31 @@ def print_slow(str):
         sys.stdout.flush()
         time.sleep(0.04)
 
-print ('PS you can terminate the game by typing : (Terminate)')
+print ('PS you can terminate the game by typing :Terminate')
 while game:
-
-        from random import randint, randrange
+        clearScreen(3)
         number = randint(1,1000)
+        print (number)
+        x = 0
 
-        for x in range(10):
-            guess = (int(input('Guess a number between 1 & 1000 : ')))
-
-
+        while x < 10:
+            x += 1
+            print ("Dit is je", (x) , "kans")
+            guess = input('Guess a number between 1 & 1000 : ')
             if guess =="Terminate":
                 print ('Game has been terminated')
-                break
+                clearScreen(1)
+                game = False 
+                x = 10
 
-            if guess == number:
+            elif int(guess) == number:
                 print_slow("You've guessed right !\n")
                 break
-
             else:
                 print_slow("You've guessed wrong ! try again !\n")
                 time.sleep (1)
 
+    
 
 
 
